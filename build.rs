fb2067d379ecdaaaeaf309c39774a3609ecb38e7
te <thrzl@icloud.com>
2024-04-13 21:57:02 -0400

    💚 lock in

--- build.rs
+++ build.rs
@@ -4,17 +4,7 @@ extern crate winres;
 
 fn main() {
     let mut res = winres::WindowsResource::new();
-    res.set_manifest(r#"
-    <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
-    <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
-        <security>
-            <requestedPrivileges>
-                <requestedExecutionLevel level="requireAdministrator" uiAccess="false" />
-            </requestedPrivileges>
-        </security>
-    </trustInfo>
-    </assembly>
-    "#);
+    res.set_manifest_file("manifest.xml");
     res.compile().unwrap();
 
     embed_resource::compile("tray.rc", embed_resource::NONE);
